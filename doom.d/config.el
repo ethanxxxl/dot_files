c0598737bea698fc378e42e1680df155ddb3caa7
Ethan Smith <ethansmith.dev@gmail.com>
2023-05-04 04:09:26 -0400

    added doom config

---
+++ doom.d/config.el
@@ -0,0 +1,166 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Ethan Smith"
+      user-mail-address "ethansmith.dev@gmail.com")
+
+;; pull changes from remote directory.
+(let ((dot-files-dir "~/dot_files/"))
+  (require 'magit)
+  (cd dot-files-dir)
+  (magit-fetch-from-upstream "https://github.com/ethanxxxl/dot_files" ""))
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Source Code Pro" :size 17 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-molokai)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'visual)
+
+;; LSP configuration
+(setq gc-cons-threshold 1600000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+;;(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+
+;;(add-hook 'js2-mode-hook #'tide-setup)
+;; configure javascript-tide checker to run after your default javascript checker
+;;(flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+
+(after! lsp-mode
+  (setq +lsp-company-backends
+        '(:separate company-capf company-yasnippet)))
+
+(load! "extra/avr-asm-autodoc-mode.el")
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(use-package lsp-grammarly
+  :ensure t
+  :hook (text-mode . (lambda ()
+                       (require 'lsp-grammarly)
+                       (lsp))))  ; or lsp-deferred
+
+(setq lsp-ui-sideline-show-code-actions t)
+(setq lsp-ui-sideline-show-hover t)
+(setq lsp-ui-sideline-show-diagnostics t)
+(setq lsp-ui-doc-enable t)
+(setq lsp-ui-doc-show-with-cursor t)
+(setq lsp-ui-doc-show-with-mouse nil)
+(setq lsp-ui-doc-delay 0.25)
+(setq lsp-ui-doc-use-webkit nil)
+(setq lsp-ui-doc-glance t)
+
+
+(defun ethan-book-mode ()
+  (interactive)
+  (setq size 100)
+  "set a variable pitch font, and fixes the window size to size,
+and centers the window."
+
+  (let ((margin-size (/ (- (window-total-width) size) 2)))
+    (set-window-margins (selected-window) margin-size margin-size)
+    (variable-pitch-mode)
+    (set-face-attribute  'line-number nil
+                         :family "Ubuntu Mono")
+    (set-face-attribute 'line-number-current-line nil
+                        :family "Ubuntu Mono")
+    (set-face-attribute 'variable-pitch nil
+                        :family "Bookerly")
+    (set-face-attribute 'org-level-1 nil
+                        :family "Source Code Pro" :height 1.4)
+    (set-face-attribute 'org-level-2 nil
+                        :family "Source Code Pro" :height 1.2)
+    (set-face-attribute 'org-level-3 nil
+                        :family "Source Code Pro" :height 1.1)
+    (setq line-spacing 10)))
+
+(defun ethan-disable-book-mode ()
+  "resets state to the way it was before book mode was enabled."
+  (interactive)
+  (set-window-margins (selected-window) 0 0)
+  (variable-pitch-mode -1)
+
+  (set-face-attribute 'line-number nil
+                      :family "Monospace")
+  (set-face-attribute 'line-number-current-line nil
+                      :family "Monospace")
+  (set-face-attribute 'variable-pitch nil
+                      :family nil)
+  (set-face-attribute 'org-level-1 nil
+                      :family nil :height nil)
+  (set-face-attribute 'org-level-2 nil
+                      :family nil :height nil)
+  (set-face-attribute 'org-level-3 nil
+                      :family nil :height nil)
+  (setq line-spacing nil))
+
+;; NOTE: you need to set variable-pitch mode first
+
+(defun ethan-hex-highlight ()
+  (font-lock-add-keywords nil '(;; Valid hex number (will highlight invalid suffix though)
+                                ("\\b0x[[:xdigit:]]+[uUlL]*\\b" . font-lock-string-face)
+                                ;; Invalid hex number
+                                ("\\b0x\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
+                                ;; Valid binary number (will highlight invalid suffix though)
+                                ("\\b0b[[:xdigit:]]+[uUlL]*\\b" . font-lock-string-face)
+                                ;; Invalid binary number
+                                ("\\b0b\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
+                                ;; Valid floating point number.
+                                ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b"
+                                 (1 font-lock-string-face) (3 font-lock-string-face))
+                                ;; Invalid floating point number.  Must be before valid decimal.
+                                ("\\b[0-9].*?\\..+?\\b" . font-lock-warning-face)
+                                ;; Valid decimal number.  Must be before octal regexes otherwise 0 and 0l
+                                ;; will be highlighted as errors.  Will highlight invalid suffix though.
+                                ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" 1 font-lock-string-face)
+                                ;; Valid octal number
+                                ("\\b0[0-7]+[uUlL]*\\b" . font-lock-string-face)
+                                ;; Floating point number with no digits after the period.  This must be
+                                ;; after the invalid numbers, otherwise it will "steal" some invalid
+                                ;; numbers and highlight them as valid.
+                                ("\\b\\([0-9]+\\)\\." (1 font-lock-string-face))
+                                ;; Invalid number.  Must be last so it only highlights anything not
+                                ;; matched above.
+                                ("\\b[0-9]\\(\\w\\|\\.\\)+?\\b" . font-lock-warning-face))))
+
+(add-hook 'asm-mode-hook 'ethan-hex-highlight)
+(add-hook 'rustic-mode-hook 'ethan-hex-highlight)
---
+++ doom.d/custom.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
+ '(custom-safe-themes
+   '("be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "028c226411a386abc7f7a0fba1a2ebfae5fe69e2a816f54898df41a6a3412bb5" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" "22a514f7051c7eac7f07112a217772f704531b136f00e2ccfaa2e2a456558d39" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(exwm-floating-border-color "#0F1019")
+ '(fci-rule-color "#767676")
+ '(highlight-tail-colors
+   ((("#172219" "color-113" "green")
+     . 0)
+    (("#112226" "color-79" "cyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0F1019" "#D85F00"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0F1019" "#79D836"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0F1019" "#767676"))
+ '(objed-cursor-color "#D83441")
+ '(org-agenda-files
+   '("~/Documents/staws/notes.org" "/home/ethan/Documents/staws/todo.org" "/home/ethan/org-basics.org" "/home/ethan/org/notes.org" "/home/ethan/org/todo.org"))
+ '(package-selected-packages '(ron-mode))
+ '(pdf-view-midnight-colors (cons "#CEDBE5" "#0D0E16"))
+ '(rustic-ansi-faces
+   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
+ '(vc-annotate-background "#0D0E16")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79D836")
+    (cons 40 "#98cd39")
+    (cons 60 "#b8c33d")
+    (cons 80 "#D8B941")
+    (cons 100 "#d89b2b")
+    (cons 120 "#d87d15")
+    (cons 140 "#D85F00")
+    (cons 160 "#ba5548")
+    (cons 180 "#9d4b90")
+    (cons 200 "#8041D8")
+    (cons 220 "#9d3ca5")
+    (cons 240 "#ba3873")
+    (cons 260 "#D83441")
+    (cons 280 "#bf444e")
+    (cons 300 "#a7555b")
+    (cons 320 "#8e6568")
+    (cons 340 "#767676")
+    (cons 360 "#767676")))
+ '(vc-annotate-very-old-color nil)
+ '(verilog-indent-level 4)
+ '(verilog-indent-level-behavioral 4)
+ '(verilog-indent-level-declaration 4)
+ '(verilog-indent-level-module 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
