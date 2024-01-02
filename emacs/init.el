966988aac2c3aa2dca1bd94a02d5e9e9ffe37f6a
Ethan Smith <ethansmith.dev@gmail.com>
2024-01-02 05:28:26 -0500

    made a ton of updates. added some key binds and packages.

--- emacs/init.el
+++ emacs/init.el
@@ -8,6 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/.config/emacs/config.org"))
+ '(org-export-backends '(ascii html icalendar latex md odt texinfo))
  '(package-selected-packages '(sly)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
