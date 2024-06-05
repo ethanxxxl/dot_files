449ccb5cb031df4066dba18497c239a69b0560a6
Ethan Smith <ethansmith.dev@gmail.com>
2024-06-04 21:26:50 -0700

    laptop updtae

--- emacs/init.el
+++ emacs/init.el
@@ -7,12 +7,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/.config/emacs/config.org"))
+ '(org-agenda-files
+   '("~/Documents/ProgrammableTanks2/plan.org" "/home/ethan/.config/emacs/config.org"))
  '(org-export-backends '(ascii html icalendar latex md odt texinfo))
- '(package-selected-packages '(sly)))
+ '(package-selected-packages '(sly))
+ '(safe-local-variable-values '((eval lsp-disconnect) (lsp-mode . -1))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
