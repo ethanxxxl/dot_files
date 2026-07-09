1333eb81cbad09949080aaad2d41d4c2172e08f6
ethanxxxl <ethansmith.dev@gmail.com>
2026-07-09 16:21:47 -0400

    fixed errors/warning on startup

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,90 @@
+;;; AUTHOR: Ethan Smith
+;;; DESCRIPTION: Emacs Config
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Load/Init Elpaca
+(setq use-package-always-ensure t)
+
+;; the following blurb was taken directly from the elpaca github
+(defvar elpaca-installer-version 0.12)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el"
+                                                (:exclude "extensions"))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process
+                                 `("git" nil ,buffer t "clone"
+                                   ,@(when-let* ((depth (plist-get order :depth)))
+                                       (list (format "--depth=%d" depth) "--no-single-branch"))
+                                   ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+(when (equal system-type 'windows-nt)
+  (elpaca-no-symlink-mode))
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode))
+
+;; Block until current queue processed.
+(elpaca-wait)
+
+;; When installing a package which modifies a form used at the top-level
+;; (e.g. a package which adds a use-package key word),
+;; use `elpaca-wait' to block until that package has been installed/configured.
+;; For example:
+;; (use-package general :demand t)
+;; (elpaca-wait)
+   
+;; Turns off elpaca-use-package-mode current declaration
+;; Note this will cause the declaration to be interpreted immediately (not deferred).
+;; Useful for configuring built-in emacs features.
+;;  (use-package emacs :ensure nil :config (setq ring-bell-function #'ignore))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs Custom Variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch-serif ((t (:box (:line-width (1 . 1) :color "gray" :style flat-button) :height 0.85 :family "Monospace")))))
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
@@ -7,8 +94,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5" default))
  '(org-agenda-files
-   '("~/Documents/ProgrammableTanks2/plan.org" "/home/ethan/.config/emacs/config.org"))
+   '("~/Documents/ProgrammableTanks2/plan.org"
+     "/home/ethan/.config/emacs/config.org"))
  '(org-export-backends '(ascii html icalendar latex md odt texinfo))
  '(package-selected-packages '(sly))
  '(safe-local-variable-values '((eval lsp-disconnect) (lsp-mode . -1))))
